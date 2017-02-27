280bbcd0c4674e98fd0db0d008b826d5d57d68ad
Denis <carriere.denis@gmail.com>
2017-02-26 21:33:46 -0500

    Remove any GeoJSON related testing - Tests are based on number arrays (typical use for PolyK).

--- test.js
+++ test.js
@@ -1,11 +1,6 @@
-const test = require('tape')
 const fs = require('fs')
+const test = require('tape')
 const path = require('path')
-const load = require('load-json-file')
-const write = require('write-json-file')
-const chunk = require('lodash.chunk')
-const turf = require('@turf/helpers')
-const featureCollection = require('@turf/helpers').featureCollection
 const PolyK = require('.')
 
 const directories = {
@@ -13,90 +8,40 @@ const directories = {
   out: path.join(__dirname, 'test', 'out') + path.sep
 }
 
-const fixtures = {}
-fs.readdirSync(directories.in).forEach(filename => {
-  const name = path.parse(filename).name
-  fixtures[name] = load.sync(directories.in + filename)
-})
-
-/**
- * Convert GeoJSON Polygon to Polygon
- *
- * @param {Feature<Polygon>} polygon
- * @returns {number[]} Array Polygon
- */
-function convertToArray (polygon) {
-  return [].concat.apply([], polygon.geometry.coordinates[0])
-}
-
-/**
- * Convert Array Polygon to FeatureCollection GeoJSON Polygon
- *
- * @param {number[]} array
- * @returns {FeatureCollection<Polygon>}
- */
-function convertToGeoJSON (array) {
-  const geojson = featureCollection([])
-  array.forEach((item) => {
-    var coords = chunk(item, 2)
-    coords.push(coords[0])
-    geojson.features.push(turf.polygon([coords]))
-  })
-  return geojson
-}
-
 test('PolyK.Slice', t => {
   // Define fixtures
-  const polygon = fixtures['polygon']
-  const line = fixtures['line']
-  const start = line.geometry.coordinates[0]
-  const stop = line.geometry.coordinates[1]
-  line.properties['stroke'] = '#f0f'
-  line.properties['stroke-width'] = 6
+  const polygon = require(directories.in + 'polygon.json')
+  const start = [134.208984375, -4.390228926463384]
+  const end = [129.0234375, -43.51668853502907]
 
   // Slice
-  const sliced = PolyK.Slice(convertToArray(polygon), start[0], start[1], stop[0], stop[1])
+  const sliced = PolyK.Slice(polygon, start[0], start[1], end[0], end[1])
 
   // Save Results
-  const results = turf.featureCollection([
-    convertToGeoJSON(sliced),
-    line
-  ])
   if (process.env.REGEN) {
-    write.sync(directories.out + 'slice.json', sliced)
-    write.sync(directories.out + 'slice-results.geojson', results)
+    fs.writeFileSync(directories.out + 'sliced.json', JSON.stringify(sliced, null, 2))
   }
 
   // Tests
-  t.deepEquals(sliced, load.sync(directories.out + 'slice.json'))
-  t.deepEquals(results, load.sync(directories.out + 'slice-results.geojson'))
+  t.deepEquals(sliced, require(directories.out + 'sliced.json'))
   t.end()
 })
 
 test('PolyK.Raycast', t => {
   // Define fixtures
-  const polygon = fixtures['polygon']
-  const start = fixtures['start'].geometry.coordinates
-  const direction = fixtures['direction'].geometry.coordinates
+  const polygon = require(directories.in + 'polygon.json')
+  const start = [135, -25]
+  const direction = [110, -10]
 
   // Raycast
-  const raycast = PolyK.Raycast(convertToArray(polygon), start[0], start[1], direction[0], direction[1])
+  const raycast = PolyK.Raycast(polygon, start[0], start[1], direction[0], direction[1])
 
   // Save Results
-  const results = turf.featureCollection([
-    polygon,
-    fixtures['start'],
-    fixtures['direction'],
-    turf.point([raycast.norm.x, raycast.norm.y], {norm: true}),
-    turf.point([raycast.refl.x, raycast.refl.y], {refl: true})
-  ])
   if (process.env.REGEN) {
-    write.sync(directories.out + 'raycast.json', raycast)
-    write.sync(directories.out + 'raycast-results.geojson', results)
+    fs.writeFileSync(directories.out + 'raycast.json', JSON.stringify(raycast, null, 2))
   }
 
   // Tests
-  t.deepEquals(raycast, load.sync(directories.out + 'raycast.json'))
-  t.deepEquals(results, load.sync(directories.out + 'raycast-results.geojson'))
+  t.deepEquals(raycast, require(directories.out + 'raycast.json'))
   t.end()
 })
